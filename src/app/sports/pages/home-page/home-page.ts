c9c88df14fed780220f112d8795c0d29c1b20356
Joel Morera <joelmoreraapaza@gmail.com>
2026-04-01 03:38:35 +0100

    fix: header good looking and errors fixed

--- src/app/sports/pages/home-page/home-page.ts
+++ src/app/sports/pages/home-page/home-page.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { Navbar } from '../../components/navbar/navbar';
 import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-home-page',
-  imports: [Header, Navbar],
+  imports: [Header],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
