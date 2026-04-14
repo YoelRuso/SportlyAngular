8fadeedb0c02d587d1183a1327b940e93f4e3c64
Bencrex <klee.benjamin6@gmail.com>
2026-04-14 21:43:25 +0100

    Apikey deleted

--- public/firebase-config.template.js
+++
@@ -1,9 +0,0 @@
-window.__firebaseConfig = {
-  apiKey: "AIzaSyDcgIPTL0b6bol7VNnlX9o3u_NOqUwSDuc",
-  authDomain: "sportlyangular.firebaseapp.com",
-  projectId: "sportlyangular",
-  storageBucket: "sportlyangular.firebasestorage.app",
-  messagingSenderId: "1097527919388",
-  appId: "1:1097527919388:web:a8334d8c4f6f5748cbf0d9",
-  measurementId: "G-DBKWBL4J06"
-};
