061ffa36b9ad94d67f9fcd9c7983055137df8827
Bencrex <klee.benjamin6@gmail.com>
2026-04-15 15:51:00 +0100

    Authentication works!

--- src/app/sports/components/login/login.ts
+++ src/app/sports/components/login/login.ts
@@ -1,9 +1,46 @@
 import { Component } from '@angular/core';
+import { Authentication } from '../../services/authentication'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  constructor(private auth: Authentication) {}
+
+  ngOnInit() {
+    this.userEmail = this.auth.getCurrentUser()?.email;
+  }
+
+  password: string = '';
+  email: string = '';
+  error: string = '';
+  loading: boolean = false;
+
+  userEmail: string | null | undefined = '';
+
+
+  async onLogin() {
+      console.log("Loginprocess started");
+    this.error = '';
+    this.loading = true;
+
+    try {
+      this.auth.login(this.email, this.password);
+      console.log("Succesfully logged in Mr.");
+      console.log("Email " + this.auth.getCurrentUser()?.email);
+      this.userEmail = this.auth.getCurrentUser()?.email;
+      console.log("EmailToken " + this.auth.getCurrentUser()?.uid);
+    }
+    catch (err: any) {
+      this.error = err.message || 'login failed :(';
+    }
+    finally {
+      console.log("Loginprocess over");
+      this.loading = false;
+    }
+  }
+}
--- src/app/sports/services/authentication.ts
+++ src/app/sports/services/authentication.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@angular/core';
+import { User, Auth, signInWithEmailAndPassword, authState, signOut } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Authentication {}
+export class Authentication {
+  user$: Observable<User | null>;
+
+  constructor(private auth: Auth) {
+    this.user$ = authState(this.auth);
+  }
+
+  login(email: string, password: string): Promise<User> {
+    return signInWithEmailAndPassword(this.auth, email, password).
+    then(cred => cred.user);
+  }
+
+  logout(): Promise<void> {
+    return signOut(this.auth);
+  }
+
+  getCurrentUser(): User | null {
+    return this.auth.currentUser;
+  }
+}
