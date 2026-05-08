fe03bda19d50c52b2a29cb1b305d2d080c8b786d
Joel Morera <joelmoreraapaza@gmail.com>
2026-05-08 16:10:19 +0100

    feat: iconic login added

---
+++ android/app/src/main/java/com/sportly/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sportly.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sportly.app',
+  appName: 'SPORTLY',
+  webDir: 'dist/SPORTLY/browser',
+  bundledWebRuntime: false,
+};
+
+export default config;
